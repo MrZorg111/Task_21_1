a4cdda3b456a45032e5e8d9f739eb2280f855339
MrZorg111 <yodji333@mail.ru>
2022-09-07 14:09:05 +0300

    Подготовка команды List.

--- main.cpp
+++ main.cpp
@@ -13,38 +13,57 @@ struct data_base{
 int main() {
     std::vector<data_base> meaning;
     std::string command, word, answer = "yes";
-    std::ofstream write;
-    std::cout << "Enter command: (add/list) ";
-    std::cin >> command;
-    if (command == "add") {
-        for (int i = 0; answer == "yes" ; i++) {
-            meaning.push_back(data_base());
-            std::cout << "Enter name: " << "\n";
-            std::cin >> word;
-            meaning[i].name = word;
-            std::cout << "Enter surname: " << "\n";
-            std::cin >> word;
-            meaning[i].surname = word;
-            std::cout << "Enter date: " << "\n";
-            std::cin >> word;
-            meaning[i].date = word;
-            std::cout << "Enter sum: " << "\n";
-            std::cin >> word;
-            meaning[i].sum = word;
-            std::cout << "Continue the program? (yes/no) ";
-            std::cin >> answer;
+    do {
+        std::cout << "Enter command: (add/list) ";
+        std::cin >> command;
+        if (command == "add") {
+            std::ofstream write;
+            for (int i = 0; answer == "yes"; i++) {
+                meaning.push_back(data_base());
+                std::cout << "Enter name: " << "\n";
+                std::cin >> word;
+                meaning[i].name = word;
+                std::cout << "Enter surname: " << "\n";
+                std::cin >> word;
+                meaning[i].surname = word;
+                std::cout << "Enter date: " << "\n";
+                std::cin >> word;
+                meaning[i].date = word;
+                std::cout << "Enter sum: " << "\n";
+                std::cin >> word;
+                meaning[i].sum = word;
+                std::cout << "Continue the program? (yes/no) ";
+                std::cin >> answer;
+            }
+            write.open("P:\\Meaning.txt");
+            for (int w = 0; w < meaning.size(); w++) {
+                write << meaning[w].name << "\t" << meaning[w].surname << "\t" << meaning[w].date << "\t"
+                      << meaning[w].sum << "\n";
+            }
+            write.close();
         }
-        write.open("P:\\Meaning.txt");
-        for (int w = 0; w < meaning.size(); w++) {
-            write << meaning[w].name << "\t" << meaning[w].surname << "\t" << meaning[w].date << "\t" << meaning[w].sum << "\n";
+        else if (command == "list") {
+            std::fstream read;
+            std::string tempo = " ";
+            int count = 0;
+            read.open("P:\\Meaning.txt");
+            if (!read.eof()) {
+                read >> tempo;
+                count++;
+                std::cout << tempo << " ";
+                if (count % 4 == 0) {
+                    std::cout << "\n";
+                }
+                read.close();
+            }
+                else {
+                std::cout << "The list is empty, enter the data. \n";
+            }
         }
-        write.close();
-    }
-    else if (command == "list") {
-
-    }
-    else {
-        std::cout << "Command error!";
-    }
-    //std::cout << meaning[0].name << "\t" << meaning[0].surname << "\t" << meaning[0].date << "\t" << meaning[0].sum;
+        else {
+            std::cout << "Command error!";
+        }
+        std::cout << "Continue the program? (yes/no) ";
+        std::cin >> answer;
+    } while (answer == "yes");
 }
